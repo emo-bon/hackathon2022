4885103aab833055dadf942e447d60ab862cb500
Cymon <cymon.cox@googlemail.com>
2022-06-21 15:41:41 +0100

    Update slurm-example.sh
    default partition is call "all"
    We dont have mem as a consumable
    We dont have email configured :)

--- input_files/slurm-example.sh
+++ input_files/slurm-example.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=<add a nine alphanumeric name here>
-#SBATCH --partition=fat
+#SBATCH --partition=all
 #SBATCH --output=out
 #SBATCH --error=err
 #$BATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=
-#SBATCH --partition=
-#SBATCH --mail-user=my-email@local.com
-#SBATCH --mail-type=ALL
-
 
 cd $SLURM_SUBMIT_DIR
 source ~/.bash_profile
