44141ff51edf8ddfed970c010ea1ecf0bd80d2e2
Haris Zafeiropoulos <haris-zaf@hcmr.gr>
2022-06-21 17:38:18 +0300

    minor edits to see some further slurm arguments

--- input_files/slurm-example.sh
+++ input_files/slurm-example.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=<add a nine alphanumeric name here>
+#SBATCH --partition=fat
 #SBATCH --output=out
 #SBATCH --error=err
 #$BATCH --nodes=1
 #SBATCH --ntasks-per-node=1
+#SBATCH --mem=
+#SBATCH --partition=
+#SBATCH --mail-user=my-email@local.com
+#SBATCH --mail-type=ALL
+
 
 cd $SLURM_SUBMIT_DIR
 source ~/.bash_profile
